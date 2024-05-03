4cc5841f715fa41d51299b788f1e15a04018a942
RakibulB <rakibulbhuiyan1973@gmail.com>
2024-05-03 14:22:16 +0100

    mathc history component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { MatchHistory } from '@/components/matchHistory';
 import SheetMobile from '@/components/sheet';
 import { Sidebar } from '@/components/sidebar';
 import Team from '@/components/teamIcon';
@@ -13,7 +14,7 @@ export default function Home() {
       <div className='h-full lg:hidden pt-3 px-2 flex flex-col items-center'>
         <SheetMobile />
       </div>
-      <main className='h-full flex-1 p-4 flex flex-col gap-y-4'>
+      <main className='h-full flex-1 pt-4 px-4 flex flex-col gap-y-4'>
         <div className='p-4 flex justify-center w-full h-[200px] lg:h-[400px] bg-[#725BF4] rounded-lg'>
           <div className='w-full lg:max-w-[560px] h-full flex justify-between items-center'>
             <Team imageUrl='/inter.png' />
@@ -27,7 +28,13 @@ export default function Home() {
           </div>
         </div>
         <p className='text-xl font-bold'>Match history</p>
-        <div className='flex-1 bg-[#1B1D37] rounded-t-2xl'></div>
+        <div className='flex-1 overflow-y-auto flex flex-col bg-[#1B1D37] rounded-t-2xl p-6'>
+          <MatchHistory/>
+          <MatchHistory/>
+          <MatchHistory/>
+          <MatchHistory/>
+          <MatchHistory/>
+        </div>
       </main>
     </div>
   );
