e900b9a1ac0afc073e4d5abc4a9277c27934ac1b
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-06-06 18:41:45 +0100

    types changed

--- types/supabase.ts
+++ types/supabase.ts
@@ -33,7 +33,6 @@ export type Database = {
         };
         Relationships: [
           {
-<<<<<<< HEAD
             foreignKeyName: "Goals_match_id_fkey";
             columns: ["match_id"];
             isOneToOne: false;
@@ -53,27 +52,6 @@ export type Database = {
             isOneToOne: false;
             referencedRelation: "Teams";
             referencedColumns: ["id"];
-=======
-            foreignKeyName: 'Goals_match_id_fkey';
-            columns: ['match_id'];
-            isOneToOne: false;
-            referencedRelation: 'Matches';
-            referencedColumns: ['id'];
-          },
-          {
-            foreignKeyName: 'goals_player_id_fkey';
-            columns: ['player_id'];
-            isOneToOne: false;
-            referencedRelation: 'Players';
-            referencedColumns: ['id'];
-          },
-          {
-            foreignKeyName: 'goals_team_id_fkey';
-            columns: ['team_id'];
-            isOneToOne: false;
-            referencedRelation: 'Teams';
-            referencedColumns: ['id'];
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
           }
         ];
       };
@@ -131,7 +109,6 @@ export type Database = {
         };
         Relationships: [
           {
-<<<<<<< HEAD
             foreignKeyName: "Team_Player_player_id_fkey";
             columns: ["player_id"];
             isOneToOne: false;
@@ -144,20 +121,6 @@ export type Database = {
             isOneToOne: false;
             referencedRelation: "Teams";
             referencedColumns: ["id"];
-=======
-            foreignKeyName: 'Team_Player_player_id_fkey';
-            columns: ['player_id'];
-            isOneToOne: false;
-            referencedRelation: 'Players';
-            referencedColumns: ['id'];
-          },
-          {
-            foreignKeyName: 'Team_Player_team_id_fkey';
-            columns: ['team_id'];
-            isOneToOne: false;
-            referencedRelation: 'Teams';
-            referencedColumns: ['id'];
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
           }
         ];
       };
@@ -166,25 +129,19 @@ export type Database = {
           id: number;
           match_id: number | null;
           name: string | null;
-<<<<<<< HEAD
-=======
           score: number;
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
         };
         Insert: {
           id?: number;
           match_id?: number | null;
           name?: string | null;
-<<<<<<< HEAD
-=======
           score?: number;
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
         };
         Update: {
           id?: number;
           match_id?: number | null;
           name?: string | null;
-<<<<<<< HEAD
+          score?: number;
         };
         Relationships: [
           {
@@ -193,24 +150,6 @@ export type Database = {
             isOneToOne: false;
             referencedRelation: "Matches";
             referencedColumns: ["id"];
-          },
-          {
-            foreignKeyName: "Teams_match_id_fkey1";
-            columns: ["match_id"];
-            isOneToOne: false;
-            referencedRelation: "Matches";
-            referencedColumns: ["id"];
-=======
-          score?: number;
-        };
-        Relationships: [
-          {
-            foreignKeyName: 'Teams_match_id_fkey';
-            columns: ['match_id'];
-            isOneToOne: false;
-            referencedRelation: 'Matches';
-            referencedColumns: ['id'];
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
           }
         ];
       };
@@ -230,47 +169,27 @@ export type Database = {
   };
 };
 
-<<<<<<< HEAD
 type PublicSchema = Database[Extract<keyof Database, "public">];
-=======
-type PublicSchema = Database[Extract<keyof Database, 'public'>];
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
 
 export type Tables<
   PublicTableNameOrOptions extends
-    | keyof (PublicSchema['Tables'] & PublicSchema['Views'])
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-<<<<<<< HEAD
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
     : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-=======
-    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
-        Database[PublicTableNameOrOptions['schema']]['Views'])
-    : never = never
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
-      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
       Row: infer R;
     }
     ? R
     : never
-<<<<<<< HEAD
   : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
       PublicSchema["Views"])
   ? (PublicSchema["Tables"] &
       PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-=======
-  : PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] &
-      PublicSchema['Views'])
-  ? (PublicSchema['Tables'] &
-      PublicSchema['Views'])[PublicTableNameOrOptions] extends {
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
       Row: infer R;
     }
     ? R
@@ -279,31 +198,19 @@ export type Tables<
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
-    | keyof PublicSchema['Tables']
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-<<<<<<< HEAD
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-=======
-    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
-    : never = never
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
       Insert: infer I;
     }
     ? I
     : never
-<<<<<<< HEAD
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
   ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-=======
-  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
-  ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
       Insert: infer I;
     }
     ? I
@@ -312,31 +219,19 @@ export type TablesInsert<
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
-    | keyof PublicSchema['Tables']
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-<<<<<<< HEAD
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-=======
-    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
-    : never = never
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
       Update: infer U;
     }
     ? U
     : never
-<<<<<<< HEAD
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
   ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-=======
-  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
-  ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
       Update: infer U;
     }
     ? U
@@ -345,10 +240,9 @@ export type TablesUpdate<
 
 export type Enums<
   PublicEnumNameOrOptions extends
-    | keyof PublicSchema['Enums']
+    | keyof PublicSchema["Enums"]
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-<<<<<<< HEAD
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
     : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
@@ -356,17 +250,3 @@ export type Enums<
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
   ? PublicSchema["Enums"][PublicEnumNameOrOptions]
   : never;
-
-export let player: Tables<"Players">;
-export let goal: Tables<"Goals">;
-export let match: Tables<"Matches">;
-export let team: Tables<"Teams">;
-=======
-    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
-    : never = never
-> = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
-  ? PublicSchema['Enums'][PublicEnumNameOrOptions]
-  : never;
->>>>>>> d498cec67848630966d7e888e925e43e62af4eaa
